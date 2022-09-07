8a1f06655af126a5634df670626563874f0a9961
Joseph Tiong <tiongchiongseng@gmail.com>
2022-09-07 18:44:59 +0800

    44

---
+++ socketHandler.js
@@ -0,0 +1,14 @@
+import { io } from "socket.io"
+window.onload = function () {
+  var socket = io();
+  var form = document.getElementById("form");
+  var input = document.getElementById("input");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    if (input.value) {
+      socket.emit("chat message", input.value);
+      input.value = "";
+    }
+  });
+};
